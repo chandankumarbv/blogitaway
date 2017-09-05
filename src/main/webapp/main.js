7f7b6c86799b5d46ee4b134141737edeeca554af
cbv <cbv@cisco.com>
2017-09-05 12:07:03 +0530

    removing react-router dep

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,5 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 import App from './components/App.jsx';
 ReactDOM.render( < App / > , document.getElementById('App'));
